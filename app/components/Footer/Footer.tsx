54b596ab08d1bea9ea29ff183811cd20d676fa07
TheGreatAxios <sawyer@dirtroad.dev>
2024-02-02 20:10:40 -0800

    Update Footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,7 +8,15 @@ export default function Footer() {
             <p className="copyright">
                 &copy; 2023 Dirt Road Development.{" "}
                 <Link className="a-link" target="_blank" to="https://dirtroad.dev">
-                    Powered by Dirt Road Development
+                    Powered by Dirt Road Development.
+                </Link>
+                <span> </span>
+                <Link className="a-link" target="_blank" to="mailto:general@dirtroad.dev?subject=Media Inquiry for sFUEL Station">
+                    Media Inquiries.
+                </Link>
+                <span> </span>
+                <Link className="a-link" target="_blank" to="mailto:code@dirtroad.dev?subject=Sponsorship for the sFUEL Station">
+                    Become a Sponsor.
                 </Link>
             </p>
         </footer>
