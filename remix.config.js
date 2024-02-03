78ab32fc41e5b768c0a0403a68cd141933fcf00a
TheGreatAxios <sawyer@dirtroad.dev>
2024-02-02 20:35:58 -0800

    Push Cloudflare Setup

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  serverBuildTarget: 'cloudflare-pages',
+  server: './server.js',
+  devServerBroadcastDelay: 1000,
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  serverModuleFormat: 'esm',
+  serverPlatform: 'browser',
+  serverBuildPath: 'functions/[[path]].js',
+  publicPath: '/build/',
   browserNodeBuiltinsPolyfill: {
     modules: {
       buffer: true,
